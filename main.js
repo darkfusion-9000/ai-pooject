74ca23738e6e3b440a207e9baad2cf70b901cb76
darkfusion-9000 <81690223+darkfusion-9000@users.noreply.github.com>
2023-01-12 11:35:59 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+rock = "";
+give = "";
+
+function preload() {
+    rock = loadSound("rock.mp3");   
+    give = loadSound("give.mp3");   
+}
+
+
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video,0,0,600,500);
+}
+
+function play() {
+    rock.play();
+    give.play();
+}
+
+function stop() {
+    song.stop();
+}
+
+
